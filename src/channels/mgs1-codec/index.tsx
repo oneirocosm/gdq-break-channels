7104e92bc27d207cd1e646043926359ccf825cc7
oneirocosm <software@oneirocosm.com>
2023-08-27 15:59:47 -0700

    feat(msg1-codec): add the background codec image
    The background codec image is static and should not change on this
    channel.  Other static assets will be added, but they depend on fonts.

--- src/channels/mgs1-codec/index.tsx
+++ src/channels/mgs1-codec/index.tsx
@@ -5,6 +5,8 @@ import { useListenFor, useReplicant } from 'use-nodecg';
 import styled from '@emotion/styled';
 import TweenNumber from '@gdq/lib/components/TweenNumber';
 
+import codecOutlineImage from './codec-outline-image.png';
+
 registerChannel('MGS1 Codec', 141, Mgs1Codec, {
 	position: 'bottomLeft',
 	site: 'GitHub',
@@ -22,6 +24,9 @@ function Mgs1Codec(props: ChannelProps) {
 
 	return (
 		<Container>
+			<CodecOutline>
+
+			</CodecOutline>
 			<TotalEl>
 				$<TweenNumber value={Math.floor(total?.raw ?? 0)} />
 			</TotalEl>
@@ -35,6 +40,14 @@ const Container = styled.div`
 	height: 100%;
 	padding: 0;
 	margin: 0;
+`;
+
+const CodecOutline = styled.div`
+	postion: absolute;
+	width: 1080px;
+	height: 320px;
+	background-image: url('${codecOutlineImage}');
+	transform: translate(6px, 6px);
 `;
 
 const TotalEl = styled.div`
