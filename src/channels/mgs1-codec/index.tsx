9d138f620ffd992fb166d0449bae83a6ba202780
oneirocosm <software@oneirocosm.com>
2023-08-27 23:52:51 -0700

    feat(mgs1-codec): add svg-based static background
    Add the background components that depend on svg. At the moment, this is
    only the volume bar.
    
    Using SVG paths should hopefully make it easy to
    light up some of the bars but not all of them. It also makes it easy to
    apply a slight blur.

--- src/channels/mgs1-codec/index.tsx
+++ src/channels/mgs1-codec/index.tsx
@@ -6,6 +6,11 @@ import styled from '@emotion/styled';
 import TweenNumber from '@gdq/lib/components/TweenNumber';
 
 import codecOutlineImage from './codec-outline-image.png';
+import {volumeBarPathData} from './volume-bar-path-data';
+
+interface VolumeBarCollectionProps {
+	barsData: string[];
+}
 
 registerChannel('MGS1 Codec', 141, Mgs1Codec, {
 	position: 'bottomLeft',
@@ -36,6 +41,9 @@ function Mgs1Codec(props: ChannelProps) {
 					<DonationDigits isLight={true} isBig={false}>!!</DonationDigits>
 					<DonationDigits isLight={true} isBig={true}>!!1.234</DonationDigits>
 				</DonationBlock>
+				<VolumeBlock>
+					<VolumeBarCollection barsData={volumeBarPathData}/>
+				</VolumeBlock>
 			</CodecOutline>
 			<TotalEl>
 				$<TweenNumber value={Math.floor(total?.raw ?? 0)} />
@@ -66,28 +74,16 @@ const ArrowLeft = styled.div`
 	left: 289px;
 	font-size: 20px;
 	color: #253c36;
-`
+`;
+
 const ArrowRight = styled.div`
 	position: absolute;
 	top: 121px;
 	left: 782px;
 	font-size: 20px;
 	color: #253c36;
-`
-const DonationBlock = styled.div`
-	position: absolute;
-	top: 144px;
-	left: 468px;
-`
-const DonationDigits = styled.span<{isLight: boolean, isBig: boolean}>`
-	position: relative;
-	font-family: DSEG7ClassicMini;
-	font-size: 40px;
-	font-size: ${({ isBig }) => isBig ? '40px' : '30px'};
-	font-weight: bold;
-	color: #253c36;
-	color: ${({ isLight }) => isLight ? '#77988c' : '#253c36'};
-`
+`;
+
 const GdqText = styled.div`
 	position: absolute;
 	top: 2px;
@@ -96,6 +92,57 @@ const GdqText = styled.div`
 	font-size: 20px;
 	color: #253c36;
 `;
+
+const DonationBlock = styled.div`
+	position: absolute;
+	top: 144px;
+	left: 468px;
+`;
+
+const DonationDigits = styled.span<{isLight: boolean, isBig: boolean}>`
+	position: relative;
+	font-family: DSEG7ClassicMini;
+	font-size: 40px;
+	font-size: ${({ isBig }) => isBig ? '40px' : '30px'};
+	font-weight: bold;
+	color: #253c36;
+	color: ${({ isLight }) => isLight ? '#77988c' : '#253c36'};
+`;
+
+const VolumeBlock = styled.div`
+	position: absolute;
+	top: 60px;
+	left: 364px;
+	width: 364px;
+	height: 142px;
+`;
+
+const VolumeBarCollection = (props: VolumeBarCollectionProps) => {
+	return (
+		<svg width='100%'
+		height='100%'
+		viewBox='14 0 1036 610'
+		preserveAspectRatio= 'none'
+		xmlns="http://www.w3.org/2000/svg">
+		<filter id="volumeBlur">
+			<feGaussianBlur stdDeviation="3" />
+				</filter>
+			{props.barsData.map(VolumeBar)}
+		</svg>
+	)
+}
+
+const VolumeBar = (path: string) => {
+	return (
+		<path
+		vectorEffect= 'non-scaling-stroke'
+		d= {path}
+		fill= '#223a2e'
+		stroke= 'none'
+		filter='url(#volumeBlur)'
+		/>
+	)
+}
 
 const TotalEl = styled.div`
 	font-family: gdqpixel;
