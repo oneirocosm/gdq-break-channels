99321ea53cd89f3f36387bc958a58951f69c2b1c
oneirocosm <software@oneirocosm.com>
2023-08-27 19:44:25 -0700

    feat(mgs1-codec): add text-based static background
    Add elements of the background that require fonts. This includes the GDQ
    title at the top of the page, the arrows on the buttons, and the
    background donation total display.
    
    As an added bonus, the dynamic foreground display of the donation total is
    present, but none of its functionality has been developed at this time.
    
    A README has been added to give credit where it is required. It has been
    updated to credit the creator of the DSEG7 Classic Mini font which is
    used to display the donation total.

---
+++ src/channels/mgs1-codec/README.md
@@ -0,0 +1,5 @@
+# MGS1 Codec
+This break channel is designed to display codec conversations in the style of MGS1.  The donation counter replaces the codec channel frequency, and the characters make comments in response to the donations received.
+
+## Credits
+The DSEG7 Classic Mini font is part of the [DSEG font family](https://www.keshikan.net/fonts-e.html).  It is created by [けしかん (Keshikan)](https://www.keshikan.net/) and its source code can be found [here](https://github.com/keshikan/DSEG).  This font is released under the [SIL OPEN FONT LICENSE Version 1.1](https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL).
--- src/channels/mgs1-codec/index.tsx
+++ src/channels/mgs1-codec/index.tsx
@@ -25,7 +25,17 @@ function Mgs1Codec(props: ChannelProps) {
 	return (
 		<Container>
 			<CodecOutline>
-
+				<GdqText>GDQ</GdqText>
+				<ArrowLeft>&#9664;</ArrowLeft>
+				<ArrowRight>&#9654;</ArrowRight>
+				<DonationBlock>
+					<DonationDigits isLight={false} isBig={false}>88</DonationDigits>
+					<DonationDigits isLight={false} isBig={true}>.888.888</DonationDigits>
+				</DonationBlock>
+				<DonationBlock>
+					<DonationDigits isLight={true} isBig={false}>!!</DonationDigits>
+					<DonationDigits isLight={true} isBig={true}>!!1.234</DonationDigits>
+				</DonationBlock>
 			</CodecOutline>
 			<TotalEl>
 				$<TweenNumber value={Math.floor(total?.raw ?? 0)} />
@@ -44,10 +54,47 @@ const Container = styled.div`
 
 const CodecOutline = styled.div`
 	postion: absolute;
-	width: 1080px;
+	width: 1084px;
 	height: 320px;
 	background-image: url('${codecOutlineImage}');
-	transform: translate(6px, 6px);
+	transform: translate(2px, 6px);
+`;
+
+const ArrowLeft = styled.div`
+	position: absolute;
+	top: 121px;
+	left: 289px;
+	font-size: 20px;
+	color: #253c36;
+`
+const ArrowRight = styled.div`
+	position: absolute;
+	top: 121px;
+	left: 782px;
+	font-size: 20px;
+	color: #253c36;
+`
+const DonationBlock = styled.div`
+	position: absolute;
+	top: 144px;
+	left: 468px;
+`
+const DonationDigits = styled.span<{isLight: boolean, isBig: boolean}>`
+	position: relative;
+	font-family: DSEG7ClassicMini;
+	font-size: 40px;
+	font-size: ${({ isBig }) => isBig ? '40px' : '30px'};
+	font-weight: bold;
+	color: #253c36;
+	color: ${({ isLight }) => isLight ? '#77988c' : '#253c36'};
+`
+const GdqText = styled.div`
+	position: absolute;
+	top: 2px;
+	left: 522px;
+	font-family: gdqpixel;
+	font-size: 20px;
+	color: #253c36;
 `;
 
 const TotalEl = styled.div`
