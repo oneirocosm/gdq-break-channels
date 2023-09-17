43313367d08d02ddc019fcf114c33bbe190cb45a
oneirocosm <software@oneirocosm.com>
2023-09-17 14:19:13 -0700

    feat(mgs1-codec): create text box component
    Create a component to display the dialogue text box at the bottom center
    of the screen.
    
    This text box is center justified when it is only one line long, but
    will left justify for longer lines. This replicates the dialogue boxes
    in MGS1.

---
+++ src/channels/mgs1-codec/components/TextBoxComponent.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+const OuterWrapper = styled.div`
+    font-family: sans-serif;
+    font-size: 20px;
+    color: white;
+    position: absolute;
+    left: 290px;
+    top: 230px;
+    width: 510px;
+`;
+
+const InnerWrapper = styled.div`
+    text-align: justify;
+    margin: 0 auto;
+    width: max-content;
+    max-width: 100%;
+`;
+
+
+interface TextBoxProps {
+    text: string;
+};
+
+export function Mgs1CodecTextBox(props: TextBoxProps) {
+    return (
+        <OuterWrapper>
+            <InnerWrapper>
+                {props.text}
+            </InnerWrapper>
+        </OuterWrapper>
+    )
+}
